3e4f6ae85477e6329b5789e67f372225986af66a
jypara-git <zhyparaabdieva@gmail.com>
2021-06-28 15:07:35 -0700

    authorise users with jwts

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -32,7 +32,19 @@ const typeDefs = gql`
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
     }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth 
+        addUser(username: String!, email: String!, password: String!): Auth 
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
 `;
+
+
 
 // export the typeDefs
 module.exports = typeDefs;
